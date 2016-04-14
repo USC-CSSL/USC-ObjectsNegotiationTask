991daed3d3b19ef0ed186e7d9bbf03544d83fefb
eunkyung1127 <eunkyung@usc.edu>
2016-04-14 15:20:55 -0700

    Updated agent strategies (soft/tough gentle/steep slopes)

---
+++ WebGames/src/edu/usc/cct/rapport/web_games/client/trading_area/agent_behaviour/decision_making/PrespecifiedSoftGentleSlopeStrategyFruits1.java
@@ -0,0 +1,58 @@
+package edu.usc.cct.rapport.web_games.client.trading_area.agent_behaviour.decision_making;
+
+import java.util.EnumMap;
+import java.util.EnumSet;
+
+import edu.usc.cct.rapport.web_games.client.trading_area.AgentEnum;
+import edu.usc.cct.rapport.web_games.client.trading_area.Proposal;
+import edu.usc.cct.rapport.web_games.client.trading_area.TradingObjectEnum;
+
+public class PrespecifiedSoftGentleSlopeStrategyFruits1 implements PlyRemainingIndexedProposalGenerator {
+
+	public Proposal getPrespecifiedPotentialCounterproposal(final EnumSet<TradingObjectEnum> setOfTradingObjectEnum, final int plyRemaining) {
+
+		final int turnsRemaining = plyRemaining / 5;
+
+		if (turnsRemaining <= 0) {
+			final IllegalStateException e = new IllegalStateException("No potential counterproposal is possible without a positive number of turns remaining in the negotiation.");
+			throw e;
+		};
+
+		final EnumMap<TradingObjectEnum, Integer> potentialCounterproposal = new EnumMap<TradingObjectEnum, Integer>(TradingObjectEnum.class);
+		final int[] prespecifiedTradingObjectCounts;
+
+		switch (turnsRemaining) {
+		// fruits negotiation: 3 items each 
+			case 1:
+				prespecifiedTradingObjectCounts = new int[]{1, 1, 2};	// item1, item2, item3
+				break;
+			case 2:
+				prespecifiedTradingObjectCounts = new int[]{1, 1, 2};
+				break;
+			case 3:
+				prespecifiedTradingObjectCounts = new int[]{0, 2, 2};
+				break;
+			case 4:
+				prespecifiedTradingObjectCounts = new int[]{2, 0, 3};
+				break;
+			case 5:
+				prespecifiedTradingObjectCounts = new int[]{1, 1, 3};
+				break;
+			case 6:
+				prespecifiedTradingObjectCounts = new int[]{3, 0, 3};
+				break;
+			default:
+				prespecifiedTradingObjectCounts = new int[]{1, 2, 3};
+		};
+
+		int tradingObjectCountIndex = 0;
+		for (final TradingObjectEnum tradingObjectEnum: setOfTradingObjectEnum) {
+			potentialCounterproposal.put(tradingObjectEnum, prespecifiedTradingObjectCounts[tradingObjectCountIndex]);
+			++tradingObjectCountIndex;
+		};
+
+		final Proposal result = new Proposal(AgentEnum.counterpart, potentialCounterproposal);
+		return result;
+	};
+
+};
---
+++ WebGames/src/edu/usc/cct/rapport/web_games/client/trading_area/agent_behaviour/decision_making/PrespecifiedToughSteepSlopeStrategyFruits7.java
@@ -0,0 +1,65 @@
+package edu.usc.cct.rapport.web_games.client.trading_area.agent_behaviour.decision_making;
+
+import java.util.EnumMap;
+import java.util.EnumSet;
+
+import edu.usc.cct.rapport.web_games.client.trading_area.AgentEnum;
+import edu.usc.cct.rapport.web_games.client.trading_area.Proposal;
+import edu.usc.cct.rapport.web_games.client.trading_area.TradingObjectEnum;
+
+public class PrespecifiedToughSteepSlopeStrategyFruits7 implements PlyRemainingIndexedProposalGenerator {
+
+	public Proposal getPrespecifiedPotentialCounterproposal(final EnumSet<TradingObjectEnum> setOfTradingObjectEnum, final int plyRemaining) {
+
+		final int turnsRemaining = plyRemaining / 5;
+
+		if (turnsRemaining <= 0) {
+			final IllegalStateException e = new IllegalStateException("No potential counterproposal is possible without a positive number of turns remaining in the negotiation.");
+			throw e;
+		};
+
+		final EnumMap<TradingObjectEnum, Integer> potentialCounterproposal = new EnumMap<TradingObjectEnum, Integer>(TradingObjectEnum.class);
+		final int[] prespecifiedTradingObjectCounts;
+
+		switch (turnsRemaining) {
+		// fruits negotiation: 3 items each 
+			case 1:
+//				prespecifiedTradingObjectCounts = new int[]{1, 1, 2};	// item1(3), item2(4), item3(5) >> 1 2 3
+				prespecifiedTradingObjectCounts = new int[]{1, 2, 1};	// item1(4), item2(5), item3(3) >> 2 3 1
+				break;
+			case 2:
+//				prespecifiedTradingObjectCounts = new int[]{1, 1, 2};
+				prespecifiedTradingObjectCounts = new int[]{1, 2, 1};
+				break;
+			case 3:
+//				prespecifiedTradingObjectCounts = new int[]{0, 2, 2};
+				prespecifiedTradingObjectCounts = new int[]{2, 2, 0};
+				break;
+			case 4:
+//				prespecifiedTradingObjectCounts = new int[]{2, 0, 3};
+				prespecifiedTradingObjectCounts = new int[]{0, 3, 2};
+				break;
+			case 5:
+//				prespecifiedTradingObjectCounts = new int[]{1, 1, 3};
+				prespecifiedTradingObjectCounts = new int[]{1, 3, 1};
+				break;
+			case 6:
+//				prespecifiedTradingObjectCounts = new int[]{3, 0, 3};
+				prespecifiedTradingObjectCounts = new int[]{0, 3, 3};
+				break;
+			default:
+//				prespecifiedTradingObjectCounts = new int[]{1, 2, 3};
+				prespecifiedTradingObjectCounts = new int[]{2, 3, 1};
+		};
+
+		int tradingObjectCountIndex = 0;
+		for (final TradingObjectEnum tradingObjectEnum: setOfTradingObjectEnum) {
+			potentialCounterproposal.put(tradingObjectEnum, prespecifiedTradingObjectCounts[tradingObjectCountIndex]);
+			++tradingObjectCountIndex;
+		};
+
+		final Proposal result = new Proposal(AgentEnum.counterpart, potentialCounterproposal);
+		return result;
+	};
+
+};
